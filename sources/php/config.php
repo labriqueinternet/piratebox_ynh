3904d3d4eb1b4a5524aa4fe15e8659fbd192d11c
Julien VAUBOURG <julien@vaubourg.com>
2015-03-15 21:51:37 +0100

    Add NAT rules

--- sources/php/config.php
+++ sources/php/config.php
@@ -1,14 +1,14 @@
 <?php
 
 
-define('DC_TITLE','DropCenter');//Titre du dropCenter
-define('DC_DESCRIPTION','File drop center kiss n\'fun');//description du dropCenter
+define('DC_TITLE','PirateBox');//Titre du dropCenter
+define('DC_DESCRIPTION','Upload and download freely');//description du dropCenter
 define('DC_LOGO','img/logo.png');//logo central du dropCenter (Si rien n'est mis, le logo par défaut apparaît)
 define('DC_LICENCE','CC BY NC SA');//License de votre dropCenter
 define('UPLOAD_FOLDER','uploads/');//chemin vers le dossier d'upload (ne pas oublier de mettre les droits d'écriture sur ce dossier)
 define('NAME_LIMIT',25);//Nombre maximal de caractères affichés pour les fichiers
 define('NAME_LIMIT_BORDER','..');//Les caractères qui s'afficheront pour signifier qu'un nom est raccourci
-define('MAX_SIZE',1000);//Taille maximale authorisée par fichier en Mo (Pensez a configurer post_max_size et upload_max_size dans le fichier php.ini de votre serveur si vous voulez uploader de gros fichiers).
+define('MAX_SIZE',1024);//Taille maximale authorisée par fichier en Mo (Pensez a configurer post_max_size et upload_max_size dans le fichier php.ini de votre serveur si vous voulez uploader de gros fichiers).
 define('FORBIDEN_FORMAT','exe,php,sh,bin,htaccess,htm,html,asp');//Les extensions interdites à l'exécution séparées par des virgules (les fichiers seront bien envoyés mais un .txt sera rajouté à l'extension afin d'empêcher les utilisateurs d'exécuter leurs fichiers sur le serveur
 define('AVATAR_DEFAULT','defaultAvatar.png'); //chemin de l'avatar par défaut
 define('AVATARFOLDER',UPLOAD_FOLDER.'avatars/'); //dossier contenant les avatars
@@ -17,8 +17,8 @@ define('RSS_MAIL','rss@mail.com');
 define('READ_FOR_ANONYMOUS',false);// Définit si les visiteurs non authentifiés peuvent lire le contenu du dropCenter (true = lecture possible, false = lecture interdite)
 define('DC_LANG','fr - Francais');//Définit la langue par défaut
 define('DIR_LANG','lang/');//Dossier des fichiers de langue
-define('MAIL',true);//Autorise les notifications par e-mail
-define('DISPLAY_DOTFILES',false);//Affiche ou non les dossiers/fichiers commençant par un point
+define('MAIL',false);//Autorise les notifications par e-mail
+define('DISPLAY_DOTFILES',true);//Affiche ou non les dossiers/fichiers commençant par un point
 define('DISPLAY_UPDATE',false);//Activer la vérification des mises-à-jour
 define('DISPLAY_AVATAR_FOLDER',false);//Afficher le dossier des avatars
 define('DEFAULT_THEME','UnderBlack');//Afficher le dossier des avatars
